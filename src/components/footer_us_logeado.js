7d6c56bd8fadfb4b9919a1de5427cc3433429bcb
efragomeni <efragomeni@gmail.com>
2025-11-11 18:37:58 -0300

    sigo agregando cosas

--- src/components/footer_us_logeado.js
+++ src/components/footer_us_logeado.js
@@ -4,16 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
     <div class="contenedor-botones-nav-footer">
         <!-- Arreglar Iconos para q sean como los del figma -->
         <button class="boton boton-nav">
-          <img src="./src/assets/icons/Home.svg" alt="" />
+          <img src="../../src/assets/icons/Home.svg" alt="" />
         </button>
         <button class="boton boton-nav">
-          <img src="./src/assets/icons/user.svg" alt="" /></button
+          <img src="../../src/assets/icons/user.svg" alt="" /></button
         ><button class="boton boton-nav">
-          <img src="./src/assets/icons/Plus.svg" alt="" /></button
+          <img src="../../src/assets/icons/Plus.svg" alt="" /></button
         ><button class="boton boton-nav">
-          <img src="./src/assets/icons/Bell.svg" alt="" /></button
+          <img src="../../src/assets/icons/Bell.svg" alt="" /></button
         ><button class="boton boton-nav">
-          <img src="./src/assets/icons/Exit.svg" alt="" />
+          <img src="../../src/assets/icons/Exit.svg" alt="" />
         </button>
       </div>
     `;
--- src/components/navbar_us_logeado.js
+++ src/components/navbar_us_logeado.js
@@ -3,23 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
   header.innerHTML = `
     <nav class="navbar">
       <div class="imagen-navbar">
-        <img src="./src/assets/img/POSTAL.png" alt="Logo Postal" />
+        <img src="../../src/assets/img/POSTAL.png" alt="Logo Postal" />
       </div>
       <div class="contenedor-botones-nav">
         <button class="boton boton-nav">
-          <img src="./src/assets/icons/Home.svg" alt="Inicio" /> Inicio
+          <img src="../../src/assets/icons/Home.svg" alt="Inicio" /> Inicio
         </button>
         <button class="boton boton-nav">
-          <img src="./src/assets/icons/user.svg" alt="Perfil" /> Perfil
+          <img src="../../src/assets/icons/user.svg" alt="Perfil" /> Perfil
         </button>
         <button class="boton boton-nav">
-          <img src="./src/assets/icons/Plus.svg" alt="Nuevo tema" /> Nuevo tema
+          <img src="../../src/assets/icons/Plus.svg" alt="Nuevo tema" /> Nuevo tema
         </button>
         <button class="boton boton-nav">
-          <img src="./src/assets/icons/Bell.svg" alt="Avisos" /> Avisos
+          <img src="../../src/assets/icons/Bell.svg" alt="Avisos" /> Avisos
         </button>
         <button class="boton boton-nav">
-          <img src="./src/assets/icons/Exit.svg" alt="Salir" /> Salir
+          <img src="../../src/assets/icons/Exit.svg" alt="Salir" /> Salir
         </button>
       </div>
     </nav>
